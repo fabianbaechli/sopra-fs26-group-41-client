c0a936a48a87b11338b67edb8840460030f9803b
fly-die <flynn.diener@gmail.com>
2026-05-19 13:59:37 +0200

    Final fix for group overlap

--- app/groups/[groupId]/page.tsx
+++ app/groups/[groupId]/page.tsx
@@ -34,7 +34,7 @@ export default function GroupOverview() {
   const [pollResultsError, setPollResultsError] = useState<string | null>(null);
 
   const [overlap, setOverlap] = useState<number | null>(null);
-  const [overlapLoading, setOverlapLoading] = useState<boolean>(false);
+  const [overlapLoading, setOverlapLoading] = useState<boolean>(true);
   const [overlapError, setOverlapError] = useState<string | null>(null);
 
   const [startingPoll, setStartingPoll] = useState(false);
